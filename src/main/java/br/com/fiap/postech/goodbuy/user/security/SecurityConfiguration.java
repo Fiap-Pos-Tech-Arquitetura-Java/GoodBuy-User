fd0302de8d595cf71c2fdce0defd8da7bdd41bae
Anderson0Wagner <anderson.wagner@gmail.com>
2024-07-07 17:50:01 -0300

    ingnorando classes de configuração do Spring na cobertura de testes.

--- src/main/java/br/com/fiap/postech/goodbuy/user/security/SecurityConfiguration.java
+++ src/main/java/br/com/fiap/postech/goodbuy/user/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.fiap.postech.goodbuy.user.security;
 
+import br.com.fiap.postech.goodbuy.user.Generated;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "br.com.fiap.postech.goodbuy.security")
+@Generated
 public class SecurityConfiguration {
 
 }
